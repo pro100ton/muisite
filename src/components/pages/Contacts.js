ed210eb4e478979ebdd036459695b82110987dfc
pro100ton <pro100ton@gmail.com>
2022-05-14 09:13:35 +0300

    Add other necessary templates for future pages

---
+++ src/components/pages/Contacts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Contacts(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Contacts;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom"
 import About from "./components/pages/About";
 import Work from "./components/pages/Work";
+import Life from "./components/pages/Life";
+import Contacts from "./components/pages/Contacts";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,8 +15,8 @@ root.render(
                 <Route path="/" element={<Navigate to="/work" />}/>
                 <Route path="work" element={<Work/>}/>
                 <Route path="about" element={<About/>}/>
-                <Route path="life" element={<About/>}/>
-                <Route path="contacts" element={<About/>}/>
+                <Route path="life" element={<Life/>}/>
+                <Route path="contacts" element={<Contacts/>}/>
             </Routes>
         </React.StrictMode>
     </BrowserRouter>
