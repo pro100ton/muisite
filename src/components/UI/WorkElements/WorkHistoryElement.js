82e1cf04d4ba19f121e90c3cfbbd6a40177e66ff
pro100ton <pro100ton@gmail.com>
2022-05-22 00:23:10 +0300

    Insert animations to work page

--- src/components/UI/WorkElements/WorkHistoryElement.js
+++ src/components/UI/WorkElements/WorkHistoryElement.js
@@ -3,8 +3,10 @@ import {Grid} from "@mui/material";
 import Box from "@mui/material/Box";
 import CardPrototype from "../CardPrototype";
 import CompanyLogoBox from "../Boxes/CompanyLogoBox";
+import {animated, useSpring} from '@react-spring/web'
 
 function WorkHistoryElement(props) {
+    const sProps = useSpring({to: {opacity: 1}, from: {opacity: 0}, delay: props.delay})
     const companyHistory = props.companyHistory;
     let companyHistoryRender = [];
     companyHistory.forEach((element, index) => {
@@ -19,41 +21,43 @@ function WorkHistoryElement(props) {
         )
     })
     return (
-        <Grid container spacing={1} columns={{xs: 1, sm: 3, md: 3}}
-            sx={{backgroundImage: `${props.color}`}}
-            rowSpacing={3}
-            justifyContent={"center"}
-            alignItems={"center"}
-        >
-            <Grid item xs={1} sm={1} md={1}
-                maxWidth={{
-                    lg: 700,
-                }}
-                minWidth={{
-                    lg: 500,
-                }}
+        <animated.div style={sProps}>
+            <Grid container spacing={1} columns={{xs: 1, sm: 3, md: 3}}
+                  sx={{backgroundImage: `${props.color}`}}
+                  rowSpacing={3}
+                  justifyContent={"center"}
+                  alignItems={"center"}
             >
-                <CompanyLogoBox companyLogo={props.companyCard.companyLogo}/>
-                <Box sx={{m: "auto", p: 1}}>
-                    <CardPrototype
-                        transparent={true}
-                        cardHeader={props.companyCard.companyName}
-                        cardMemo={props.companyCard.companyObjective}
-                        cardBody={props.companyCard.companyDescription}
-                        link={props.companyCard.companySiteLink}
-                    />
-                </Box>
+                <Grid item xs={1} sm={1} md={1}
+                      maxWidth={{
+                          lg: 700,
+                      }}
+                      minWidth={{
+                          lg: 500,
+                      }}
+                >
+                    <CompanyLogoBox companyLogo={props.companyCard.companyLogo}/>
+                    <Box sx={{m: "auto", p: 1}}>
+                        <CardPrototype
+                            transparent={true}
+                            cardHeader={props.companyCard.companyName}
+                            cardMemo={props.companyCard.companyObjective}
+                            cardBody={props.companyCard.companyDescription}
+                            link={props.companyCard.companySiteLink}
+                        />
+                    </Box>
+                </Grid>
+                <Grid item xs={1} sm={2} md={2}
+                      maxWidth={{
+                          lg: 700,
+                      }}
+                      minWidth={{
+                          lg: 500,
+                      }}>
+                    {companyHistoryRender}
+                </Grid>
             </Grid>
-            <Grid item xs={1} sm={2} md={2}
-                  maxWidth={{
-                      lg: 700,
-                  }}
-                  minWidth={{
-                      lg: 500,
-                  }}>
-                {companyHistoryRender}
-            </Grid>
-        </Grid>
+        </animated.div>
     );
 }
 
--- src/components/pages/work/WorkHistory.js
+++ src/components/pages/work/WorkHistory.js
@@ -4,17 +4,19 @@ import {workHistory} from "../../../constants/WorkData";
 
 function WorkHistory(props) {
     let workRender = [];
-
+    let startDelay = 0;
     workHistory.forEach((element, index) => {
         workRender.push(
             <WorkHistoryElement
                 key={index}
+                delay={startDelay}
                 companyCard={element.companyCard}
                 companyHistory={element.companyHistory}
                 companyLogo={element.companyCard.companyLogo}
                 color={element.companyColor}
             />
         )
+        startDelay += 500
     })
 
 
